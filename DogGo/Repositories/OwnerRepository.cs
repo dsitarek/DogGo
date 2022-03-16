8e422354ca6316e654a34d3f7fee5086b9c45ae2
Daniel Sitarek <deltasierra777@gmail.com>
2022-03-15 19:29:23 -0500

    added feature to display owners dogs as table on owner details page

--- DogGo/Repositories/OwnerRepository.cs
+++ DogGo/Repositories/OwnerRepository.cs
@@ -74,11 +74,44 @@ namespace DogGo.Repositories
                                 Address = reader.GetString(reader.GetOrdinal("Address")),
                                 NeighborhoodId = reader.GetInt32(reader.GetOrdinal("NeighborhoodId")),
                                 Phone = reader.GetString(reader.GetOrdinal("Phone")),
+                                Dogs = getDogsByOwnerId(Id)
                             };
 
                             return owner;
                         }
                         else return null;
+                    }
+                }
+            }
+        }
+
+        public List<Dog> getDogsByOwnerId(int id)
+        {
+            using(SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand command = conn.CreateCommand())
+                {
+                    command.CommandText = @"
+                                            SELECT Id, Name, OwnerId, Breed
+                                            FROM Dog
+                                            WHERE OwnerId = @id";
+                    command.Parameters.AddWithValue("@id", id);
+                    using(SqlDataReader reader = command.ExecuteReader())
+                    {
+                        List<Dog> dogs = new List<Dog>();
+                        while(reader.Read())
+                        {
+                            Dog dog = new Dog()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Name = reader.GetString(reader.GetOrdinal("Name")),
+                                OwnerId = reader.GetInt32(reader.GetOrdinal("OwnerId")),
+                                Breed = reader.GetString(reader.GetOrdinal("Breed"))
+                            };
+                            dogs.Add(dog);
+                        }
+                        return dogs;
                     }
                 }
             }
